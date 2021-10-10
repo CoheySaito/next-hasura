0b4d426d7c4952bf0134525337c24ebfa03b0cd6
DESKTOP-V6J970A\cohey <cowhey3110@gmail.com>
2021-10-10 21:50:21 +0900

    implement firebase Auth #4

---
+++ src/components/Auth.tsx
@@ -0,0 +1,192 @@
+import React from 'react';
+import { useFirebaseAuth } from '../hooks/useFirebaseAuth';
+import firebase from '../../firebaseConfig';
+import NextLink from 'next/link';
+
+import {
+  Box,
+  Button,
+  Center,
+  chakra,
+  FormControl,
+  FormLabel,
+  Heading,
+  HStack,
+  Input,
+  Link,
+  Stack,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { AiOutlineUserAdd, AiOutlineLogin } from 'react-icons/ai';
+import { BiLinkExternal } from 'react-icons/bi';
+import DividerWithText from './DividerWithText';
+import { useLogout } from '../hooks/useLogout';
+import { useRouter } from 'next/router';
+
+const Auth: React.VFC = () => {
+  const {
+    email,
+    password,
+    isLogin,
+    emailChange,
+    pwChange,
+    resetInput,
+    toggleMode,
+    authUser,
+  } = useFirebaseAuth();
+
+  const currentUser = firebase.auth().currentUser;
+  const { logout } = useLogout();
+  const router = useRouter();
+
+  const logoutHandler = () => {
+    logout();
+    resetInput();
+    router.push('/');
+  };
+
+  return (
+    <Box
+      bg={useColorModeValue('gray.50', 'inherit')}
+      minH="100vh"
+      py="12"
+      px={{ base: '4', lg: '8' }}
+    >
+      <Box maxW="md" mx="auto">
+        <Heading textAlign="center" size="xl" fontWeight="extrabold">
+          Sign in to your account
+        </Heading>
+        <HStack
+          mt="4"
+          mb="8"
+          align="center"
+          maxW="md"
+          fontWeight="medium"
+          justifyContent="center"
+          onClick={toggleMode}
+          cursor="pointer"
+        >
+          <Text
+            as="span"
+            color="blue.500"
+            transition="all .4s ease-out"
+            _hover={{
+              color: 'blue.400',
+            }}
+          >
+            {isLogin ? 'Create new account' : 'Sign In'}
+          </Text>
+          <Text color="rgb(43, 108, 176)">
+            {isLogin ? (
+              <AiOutlineUserAdd size={24} />
+            ) : (
+              <AiOutlineLogin size={24} />
+            )}
+          </Text>
+        </HStack>
+        <Box
+          bg={useColorModeValue('white', 'gray.700')}
+          py="8"
+          px={{ base: '4', md: '10' }}
+          shadow="base"
+          rounded={{ sm: 'lg' }}
+        >
+          <chakra.form onSubmit={authUser}>
+            <Stack spacing="6">
+              <FormControl id="email">
+                <FormLabel>Email address</FormLabel>
+                <Input
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  value={email}
+                  onChange={emailChange}
+                />
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel>password</FormLabel>
+                <Input
+                  name="password"
+                  type="password"
+                  autoComplete="password"
+                  required
+                  value={password}
+                  onChange={pwChange}
+                />
+              </FormControl>
+              <Button
+                type="submit"
+                colorScheme={isLogin ? 'blue' : 'pink'}
+                size="lg"
+                fontSize="md"
+                transition="all .4s ease-out"
+              >
+                {isLogin ? 'Sign in' : 'Register'}
+              </Button>
+            </Stack>
+          </chakra.form>
+          {currentUser && (
+            <>
+              <DividerWithText mt="6">
+                <Text as="span" fontWeight="bold">
+                  Login User:
+                </Text>
+                {currentUser.email}
+              </DividerWithText>
+              <Link as={NextLink} href="/trial">
+                <HStack
+                  justifyContent="center"
+                  alignItems="center"
+                  mt={{ base: '2', md: '4' }}
+                >
+                  <Text
+                    fontSize="xl"
+                    cursor="pointer"
+                    textAlign="center"
+                    color={'blue.500'}
+                    _hover={{
+                      textDecoration: 'none',
+                      color: 'blue.400',
+                    }}
+                  >
+                    to task page
+                  </Text>
+                  <Text
+                    fontSize="xl"
+                    cursor="pointer"
+                    textAlign="center"
+                    color={'blue.500'}
+                    _hover={{
+                      textDecoration: 'none',
+                      color: 'blue.400',
+                    }}
+                  >
+                    <BiLinkExternal />
+                  </Text>
+                </HStack>
+              </Link>
+              <Center>
+                <Button
+                  colorScheme="teal"
+                  size="md"
+                  fontWeight="extrabold"
+                  py={{ md: '4' }}
+                  px="6"
+                  mt="4"
+                  variant="outline"
+                  borderWidth="2px"
+                  onClick={logoutHandler}
+                >
+                  Logout
+                </Button>
+              </Center>
+            </>
+          )}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+export default Auth;
---
+++ src/hooks/useFirebaseAuth.ts
@@ -0,0 +1,64 @@
+import firebase from '../../firebaseConfig';
+import { ChangeEvent, FormEvent, useCallback, useState } from 'react';
+import { useRouter } from 'next/router';
+
+export const useFirebaseAuth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLogin, setIsLogin] = useState(true);
+
+  const router = useRouter();
+
+  //カスタムフック内の関数はuseCallbackでメモ化
+  const emailChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  }, []);
+
+  const pwChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  }, []);
+
+  const resetInput = useCallback(() => {
+    setEmail('');
+    setPassword('');
+  }, []);
+
+  //stateを参照している場合は、第2引数に指定
+  const toggleMode = useCallback(() => {
+    setIsLogin(!isLogin);
+  }, [isLogin]);
+
+  const authUser = useCallback(
+    async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (isLogin) {
+        try {
+          await firebase.auth().signInWithEmailAndPassword(email, password);
+        } catch (error) {
+          alert(error.message);
+        }
+        resetInput();
+      } else {
+        try {
+          await firebase.auth().createUserWithEmailAndPassword(email, password);
+        } catch (error) {
+          alert(error.message);
+        }
+        resetInput();
+        router.push('/');
+      }
+    },
+    [email, password, isLogin],
+  );
+
+  return {
+    email,
+    password,
+    isLogin,
+    emailChange,
+    pwChange,
+    resetInput,
+    toggleMode,
+    authUser,
+  };
+};
